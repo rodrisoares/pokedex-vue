535f0b5324199f875441081a884abd1442b4adb7
Rodrigo Soares <diihhsoares2@gmail.com>
2023-06-22 19:37:18 -0300

    criando estrutura do projeto

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">   
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+body {
+    margin: 0;
+    padding: 0;
+}
+   
+</style>
